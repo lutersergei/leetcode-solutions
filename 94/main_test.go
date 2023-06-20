17d2ed93a8baa8f166b1d40774e6048264eef49f
luter.sergey <luter.sergey@togizu.com>
2023-06-20 11:39:36 +0600

    add 94 `inorder traversal` add benchmark result

--- 94/main_test.go
+++ 94/main_test.go
@@ -36,6 +36,14 @@ var t = &TreeNode{
 	},
 }
 
+/*
+cpu: Intel(R) Core(TM) i7-8550U CPU @ 1.80GHz
+BenchmarkAppend
+BenchmarkAppend-8        1274215               897.8 ns/op           784 B/op         29 allocs/op
+BenchmarkAppend2
+BenchmarkAppend2-8       1291267               911.4 ns/op           784 B/op         29 allocs/op
+*/
+
 func BenchmarkAppend(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = inorderTraversal(t)
